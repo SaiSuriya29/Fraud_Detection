ae666aaff90580f6e58b1649b55e2b4e55e2539f
SaiSuriya29 <thamminainasaikiran2907@gmail.com>
2024-06-06 11:55:00 +0530

    added 3 new ml model code

--- ML_model.py
+++ ML_model.py
@@ -1,1 +1,10 @@
 print('adding all ML Related code here')
+
+def model1():
+    print('creating ML model1')
+
+def model2():
+    print('creating ML model2')
+
+def model3():
+    print('creating ML model3')
