44e8666acd7ad35c2ba7df3757de00223b444cb4
SaiSuriya29 <thamminainasaikiran2907@gmail.com>
2024-06-06 11:10:59 +0530

    added a new function

--- data_proceesing.py
+++ data_proceesing.py
@@ -1,1 +1,8 @@
 print('Adding all the data proceesing code related to fraud detection')
+
+a = 5
+b= 6
+def sum(a,b):
+    c = a+b
+    return a
+print('Added a new function for sum which is wrong')
